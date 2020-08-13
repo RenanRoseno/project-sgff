3910fc845c26bf875d241b8c54bdbbec6d36e80b
Renan Roseno <rosenorenan@gmail.com>
2020-08-13 18:20:15 -0300

    Commit inicial, configurações do projeto

---
+++ src/main/java/com/spring/sgff/SgffApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sgff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sgff/SgffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sgff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
