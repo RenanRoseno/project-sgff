aa5ee027e479fbc1f1b2bfb391a4cfdf502049e2
Renan Roseno <rosenorenan@gmail.com>
2020-08-24 16:03:41 -0300

    Criação de controller para a aplicação

---
+++ src/main/java/com/spring/sgff/controllers/SgffController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.sgff.controllers;
+
+import com.spring.sgff.models.Funcionarios;
+import com.spring.sgff.service.SgffService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+/**
+ *
+ * @author rosen
+ */
+@Controller
+public class SgffController {
+    @Autowired
+    
+    SgffService sgffservice;
+    
+    @RequestMapping(value = "/funcionarios", method = RequestMethod.GET)
+    
+    public ModelAndView getFuncionarios(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("Funcionarios");
+        
+        List<Funcionarios> funcionarios = sgffservice.findAll();
+        mv.addObject("funcionarios", funcionarios);
+        
+        return mv;      
+    }
+    
+}
