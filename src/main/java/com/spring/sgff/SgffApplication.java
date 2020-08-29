13245f40115094e8070d162f72899be775820f83
Renan Roseno <rosenorenan@gmail.com>
2020-08-28 22:39:12 -0300

    Alterações para renderizar funcionários na página

--- src/main/java/com/spring/sgff/SgffApplication.java
+++ src/main/java/com/spring/sgff/SgffApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SgffApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SgffApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SgffApplication.class, args);
+    }
 
 }
