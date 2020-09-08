1624400b8f5975ae49a2ae81ffa8d412cce81ef1
Renan Roseno <rosenorenan@gmail.com>
2020-09-08 13:41:25 -0300

    Especificando rota para exibir funcionário específico

--- src/main/java/com/spring/sgff/controllers/SgffController.java
+++ src/main/java/com/spring/sgff/controllers/SgffController.java
@@ -10,11 +10,10 @@ import com.spring.sgff.service.SgffService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-
 
 /**
  *
@@ -22,20 +21,33 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class SgffController {
+
     @Autowired
-    
+
     SgffService sgffservice;
-    
+
     @RequestMapping(value = "/funcionarios", method = RequestMethod.GET)
-    
-    public ModelAndView getFuncionarios(){
+
+    public ModelAndView getFuncionarios() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("Funcionarios");
-        
+
         List<Funcionarios> funcionarios = sgffservice.findAll();
         mv.addObject("funcionarios", funcionarios);
-        
-        return mv;      
+
+        return mv;
     }
-    
+
+    @RequestMapping(value = "/funcionarios/{id}", method = RequestMethod.GET)
+
+    public ModelAndView getFuncionariosDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("funcionarioDetails");
+
+        //Funcionarios funcionario = sgffservice.findById(id);
+        //mv.addObject("funcionario", funcionario);
+
+        return mv;
+    }
+
 }
