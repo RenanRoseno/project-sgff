711a87e7e74c99d09791fd88dac521f9c9c5c49a
Renan Roseno <rosenorenan@gmail.com>
2020-10-06 16:17:52 -0300

    Adesão da função pesquisar por CPF

--- src/main/java/com/spring/sgff/repository/SgffRepository.java
+++ src/main/java/com/spring/sgff/repository/SgffRepository.java
@@ -7,10 +7,12 @@ package com.spring.sgff.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.spring.sgff.models.Funcionarios;
+import org.springframework.data.jpa.repository.Query;
 /**
  *
  * @author rosen
  */
 public interface SgffRepository extends JpaRepository<Funcionarios,Long>{
-    
+    @Query("SELECT f FROM Funcionarios f WHERE f.cpf = ?1")
+    Funcionarios findByCpf(String cpf);
 }
