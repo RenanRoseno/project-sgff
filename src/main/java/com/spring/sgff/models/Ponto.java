c0ef4a70d597f630ac8842e1a6965dbd255f9768
Renan Roseno <rosenorenan@gmail.com>
2020-10-06 00:30:30 -0300

    Crianção da model de ponto

---
+++ src/main/java/com/spring/sgff/models/Ponto.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.sgff.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+/**
+ *
+ * @author rosen
+ */
+@Entity 
+@Table(name="Pontos")
+
+public class Ponto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+    private LocalDate data;
+     
+    @NotBlank
+    private String horarioEntrada;
+    
+    @NotBlank
+    private String horarioSaida;
+            
+    private int falta;
+    
+    private Long id_funcionario;
+
+    public Long getId_funcionario() {
+        return id_funcionario;
+    }
+
+    public void setId_funcionario(Long id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getHorarioEntrada() {
+        return horarioEntrada;
+    }
+
+    public void setHorarioEntrada(String horarioEntrada) {
+        this.horarioEntrada = horarioEntrada;
+    }
+
+    public String getHorarioSaida() {
+        return horarioSaida;
+    }
+
+    public void setHorarioSaida(String horarioSaida) {
+        this.horarioSaida = horarioSaida;
+    }
+
+    public int getFalta() {
+        return falta;
+    }
+
+    public void setFalta(int falta) {
+        this.falta = falta;
+    }
+    
+    
+    
+    
+    
+}
