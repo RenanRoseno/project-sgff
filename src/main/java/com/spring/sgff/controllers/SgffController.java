122808c0b806f2fa2e22290672986850c22e2473
Matheus Marques <matheus-marques@LAPTOP-K062DHUE.localdomain>
2020-10-06 00:15:38 -0300

    Função de deletar funcionário adicionada

--- src/main/java/com/spring/sgff/controllers/SgffController.java
+++ src/main/java/com/spring/sgff/controllers/SgffController.java
@@ -14,7 +14,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,20 +31,19 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class SgffController {
 
     @Autowired
-
     SgffService sgffservice;
-    
+
+    // Registrar Ponto
     @RequestMapping(value = "/registrarponto/{param}", method = RequestMethod.POST)
-    public String registrarPonto(@PathVariable("id") String param){
+    public String registrarPonto(@PathVariable("id") String param) {
         return "redirect:/";
     }
 
-
+    // Rota de que contém a lista de todos os funcionários
     @RequestMapping(value = "/funcionarios", method = RequestMethod.GET)
-
     public ModelAndView getFuncionarios() {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("Funcionarios");
+        mv.setViewName("funcionarios");
 
         List<Funcionarios> funcionarios = sgffservice.findAll();
         mv.addObject("funcionarios", funcionarios);
@@ -49,8 +51,8 @@ public class SgffController {
         return mv;
     }
 
+    // Listando o funcionário pelo id
     @RequestMapping(value = "/funcionarios/{id}", method = RequestMethod.GET)
-
     public ModelAndView getFuncionariosDetails(@PathVariable("id") long id) {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("funcionarioDetails");
@@ -66,20 +68,28 @@ public class SgffController {
         return "FuncionarioForm";
     }
 
+    // Cria um novo funcionário
     @RequestMapping(value = "/newfuncionario/", method = RequestMethod.POST)
-    public String saveFuncionario(@Valid Funcionarios funcionario, BindingResult result, RedirectAttributes attributes) {
+    public String saveFuncionario(@Valid Funcionarios funcionario, BindingResult result,
+            RedirectAttributes attributes) {
         if (result.hasErrors()) {
             attributes.addFlashAttribute("mensagem", "Verifique os dados digitados");
             return "redirect:/newfuncionario/";
         }
 
         funcionario.setDataAdmissao(LocalDate.now());
-
         sgffservice.save(funcionario);
-
         return "redirect:/funcionarios/";
     }
 
+    // Deleta um funcionário pelo id
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteFuncionario(@PathVariable("id") long id) {
+        sgffservice.deleteFuncionario(id);
+        return "redirect:/funcionarios/";
+    }
+
+    // Logout session
     @RequestMapping("logout")
     public String logout(HttpSession session) {
         session.invalidate();
