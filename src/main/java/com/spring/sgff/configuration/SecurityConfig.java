2e1a3bb7ed2cfe8d5770fc5c752b2ff09d97fd77
Renan Roseno <rosenorenan@gmail.com>
2020-10-08 16:42:01 -0300

    Implementação de autenticação via JPA diretamente do banco de dados

--- src/main/java/com/spring/sgff/configuration/SecurityConfig.java
+++ src/main/java/com/spring/sgff/configuration/SecurityConfig.java
@@ -5,12 +5,18 @@
  */
 package com.spring.sgff.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -21,22 +27,37 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String[] AUTH_LIST = {"/", "/registrarponto/"}; 
+    private static final String[] AUTH_LIST = {"/", "/registrarponto/"};
 
+    @Autowired
+    private UsuarioServiceImpl userService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+
+        return authProvider;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(AUTH_LIST).permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
                 .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("{noop}123").roles("ADMIN");
+        //auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("ADMIN");
+        auth.authenticationProvider(authenticationProvider());
+
     }
 
     @Override
