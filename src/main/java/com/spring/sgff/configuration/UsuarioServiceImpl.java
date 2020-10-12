98ad70494c96e4ca05f0da76f0aeaee54d48bc6c
Renan Roseno <rosenorenan@gmail.com>
2020-10-12 13:43:01 -0300

    Adesão de novo retorno na função de logar, para retornaro tipo do usuário

--- src/main/java/com/spring/sgff/configuration/UsuarioServiceImpl.java
+++ src/main/java/com/spring/sgff/configuration/UsuarioServiceImpl.java
@@ -2,13 +2,16 @@ package com.spring.sgff.configuration;
 
 import com.spring.sgff.models.Usuario;
 import com.spring.sgff.repository.UsuarioRepository;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Transactional
 public class UsuarioServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -22,7 +25,7 @@ public class UsuarioServiceImpl implements UserDetailsService {
             throw new UsernameNotFoundException("Usuário Inválido");
         }
 
-        return usuario;
+        return new User(usuario.getLogin(), usuario.getSenha(), true, true, true, true, usuario.getAuthorities());
     }
 
 }
